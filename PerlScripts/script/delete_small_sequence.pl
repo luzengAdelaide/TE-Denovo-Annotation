c775aba2c71d8039078a9d93fc820eda476a543c
Lu Zeng <a1635743@l01.phoenix.adelaide.edu.au>
2016-11-17 18:03:07 +1030

    add files

--- PerlScripts/script/delete_small_sequence.pl
+++ PerlScripts/script/delete_small_sequence.pl
@@ -18,17 +18,17 @@ my $seq2;
 my $head2;
 my ($chr,$start,$end,$sym,$gid);
 my $out1 = "short.".$file;
-my $out2 = "1000bp.".$file;
-my $out3 = "2000bp.".$file;
-my $out4 = "3000bp.".$file;
-my $out5 = "4000bp.".$file;
-my $out6 = "5000bp.".$file;
+my $out2 = "2000bp.".$file;
+my $out3 = "3000bp.".$file;
+my $out4 = "4000bp.".$file;
+#my $out5 = "4000bp.".$file;
+#my $out6 = "5000bp.".$file;
 open(OUTA,">$out1");
 open(OUTB,">$out2");
 open(OUTC,">$out3");
 open(OUTD,">$out4");
-open(OUTE,">$out5");
-open(OUTF,">$out6");
+#open(OUTE,">$out5");
+#open(OUTF,">$out6");
 
 $/=">";
 while (<IN>){
@@ -39,12 +39,13 @@ while (<IN>){
     $head =~s/\>//;
     $seq2 = $tmp[1];
 #    $seq2 =~s/\n//g;
-    print OUTA "\>$_", if length($seq2) < 1000;
-    print OUTB "\>$_", if length($seq2) >1000 && length($seq2) < 2000;
-    print OUTC "\>$_", if length($seq2) >2000 && length($seq2) < 3000;
-    print OUTD "\>$_", if length($seq2) >3000 && length($seq2) < 4000;
-    print OUTE "\>$_", if length($seq2) >4000 && length($seq2) < 5000;
-    print OUTF "\>$_", if length($seq2) > 5000;
+    print OUTA "\>$_", if length($seq2) < 2000;
+    print OUTB "\>$_", if length($seq2) >=2000 && length($seq2) < 3000;
+    print OUTC "\>$_", if length($seq2) >=3000 && length($seq2) < 4000;
+    print OUTD "\>$_", if length($seq2) > 4000;
+ #   print OUTD "\>$_", if length($seq2) >=3000 && length($seq2) < 4000;
+ #   print OUTE "\>$_", if length($seq2) >=4000 && length($seq2) < 5000;
+ #   print OUTF "\>$_", if length($seq2) >= 5000;
 }
 
 close IN;
