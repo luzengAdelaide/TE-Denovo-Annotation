a3039524e84d2bbc4b3465b6fed7748fc123ece6
Lu Zeng <a1635743@l01.phoenix.adelaide.edu.au>
2016-12-19 10:29:31 +1030

    polish codes

--- PerlScripts/script/reportsJ3.pl
+++ PerlScripts/script/reportsJ3.pl
@@ -5,7 +5,7 @@ my $dataDir = "./";
 my $ECrit = 0.00001;
 
 #print "Enter name of file listing libraries to process: ";
-my $libs = '/home/a1635743/libs.txt/libs.txt';
+my $libs = '/home/a1635743/libs.txt';
 
 open (INFILE, "$libs") || die "You dope, no input file by that name, try again...";
  
@@ -17,7 +17,7 @@ while (<INFILE>) {
 #  system ("/home/a1635743/report_run/piler -annot $_.gff -rep $_.spwb.gff -out $_.spwbannot.gff");
 #  system ("python /home/a1635743/report_run/gff_pretty.py $_.spwbannot.gff > $_.spwbannot.txt");
 
-#  system ("blastx /home/a1635743/BlastDB/GB_TE.new $dataDir$_ -gspmax=1 -E $ECrit -B 1 -V 1 -cpus=4 > $_.tewb");
+#  system ("blastx /home/a1635743/BlastDB/GB_TE.new $dataDir$_ -gspmax=1 -E $ECrit -B 1 -V 1 -cpus=32 > $_.tewb");
 #  system ("python /home/a1635743/report_run/wublastx2gff.py $_.tewb > $_.tewb.gff");
 #  system ("python /home/a1635743/report_run/gff_pretty.py $_.tewb.gff > $_.tewb.txt");
 #  system ("/home/a1635743/report_run/piler -annot $dataDir$_.gff -rep $_.tewb.gff -out $_.tewbannot.gff");
